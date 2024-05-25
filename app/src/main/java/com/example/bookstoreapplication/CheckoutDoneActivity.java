dd4e667213a7d7ce7a2caa679ea079f2cb61ed0b
deyanuz <zunayedofficial@gmail.com>
2024-05-25 14:31:30 +0600

    checkout done activity added

--- app/src/main/java/com/example/bookstoreapplication/CheckoutDoneActivity.java
+++ app/src/main/java/com/example/bookstoreapplication/CheckoutDoneActivity.java
@@ -1,24 +1,53 @@
 package com.example.bookstoreapplication;
 
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class CheckoutDoneActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CheckoutDoneActivity extends BaseActivity {
+    private Button button;
+    EditText phoneNo;
+    FloatingActionButton callbtn;
+    static int PERMISSION_code=100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_checkout_done);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button=(Button) findViewById(R.id.Browse);
+        phoneNo=findViewById(R.id.editTextPhone);
+        callbtn=findViewById(R.id.callbtn);
+        if(ContextCompat.checkSelfPermission(CheckoutDoneActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(CheckoutDoneActivity.this,new String[]{Manifest.permission.CALL_PHONE},PERMISSION_code);
+        }
+        callbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneno=phoneNo.getText().toString();
+                Intent i=new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:"+phoneNo));
+                startActivity(i);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(CheckoutDoneActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
